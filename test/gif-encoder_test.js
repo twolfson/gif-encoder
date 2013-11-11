0c516771df6e0bcb93549d84d5097dc037d89401
Todd Wolfson <todd@twolfson.com>
2013-11-11 04:49:39 -0800

    Filling out tests

--- test/gif-encoder_test.js
+++ test/gif-encoder_test.js
@@ -1,11 +1,20 @@
-var GifEncoder = require('../lib/GifEncoder.js');
+var GifEncoder = require('../lib/GIFEncoder.js');
 
 describe('GifEncoder', function () {
   before(function () {
-
+    this.gif = new GifEncoder();
   });
 
-  it('', function () {
+  describe('encoding a checkboard', function () {
+    before(function () {
+      var pixels = require('./test-files/checkboard-pixels.json');
+      this.gif.addFrame(pixels);
+    });
 
+    it('generates the expected bytes', function () {
+      // TODO: Output bytes to file
+      // var expectedBytes = require('./expected-files/checkboard.gif');
+      console.log(this.gif.out);
+    });
   });
 });
