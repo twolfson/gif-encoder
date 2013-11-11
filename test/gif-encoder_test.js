df9f638fac877de02cbab342248db56b19f6a1f6
Todd Wolfson <todd@twolfson.com>
2013-11-11 05:44:58 -0800

    Moved test to streaming collection

--- test/gif-encoder_test.js
+++ test/gif-encoder_test.js
@@ -2,9 +2,23 @@ var assert = require('assert');
 var fs = require('fs');
 var GifEncoder = require('../lib/GIFEncoder.js');
 
+// TODO: Test multiple frames
 describe('GifEncoder encoding a checkerboard', function () {
-  before(function () {
+  before(function (done) {
+    // Generate a new image
     var gif = new GifEncoder(10, 10);
+
+    // Collect output
+    var data = [];
+    this.data = data;
+    gif.on('data', function (val) {
+      data.push(val);
+    });
+    gif.on('end', function () {
+      done();
+    });
+
+    // Write content
     var pixels = require('./test-files/checkerboard-pixels.json');
     gif.writeHeader();
     gif.addFrame(pixels);
@@ -20,8 +34,7 @@ describe('GifEncoder encoding a checkerboard', function () {
     var expectedBytes = fs.readFileSync(__dirname + '/expected-files/checkerboard.gif');
 
     // Grab the actual content
-    var page = this.gif.out.pages[0];
-    var actualBytes = new Buffer([].slice.call(page));
+    var actualBytes = new Buffer(this.data);
 
     // DEV: Write out actual file to expected file
     if (process.env.DEBUG_TEST) {
