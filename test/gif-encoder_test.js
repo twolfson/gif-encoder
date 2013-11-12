d7da347088182971f06bbc151d9f69b400d3120c
Todd Wolfson <todd@twolfson.com>
2013-11-11 23:57:12 -0800

    Moved to streams2 for sexy tests =3

--- test/gif-encoder_test.js
+++ test/gif-encoder_test.js
@@ -8,19 +8,12 @@ describe('GifEncoder encoding a checkerboard', function () {
     // Generate a new image
     var gif = new GifEncoder(10, 10);
 
-    // Collect output
-    var buffers = [];
-    this.buffers = buffers;
-    gif.on('data', function (val) {
-      buffers.push(val);
-    });
-    gif.on('end', done);
-
     // Write content
     var pixels = require('./test-files/checkerboard-pixels.json');
     gif.writeHeader();
     gif.addFrame(pixels);
     gif.finish();
+    gif.on('readable', done);
 
     // Save gif for later
     this.gif = gif;
@@ -32,7 +25,7 @@ describe('GifEncoder encoding a checkerboard', function () {
     var expectedBytes = fs.readFileSync(__dirname + '/expected-files/checkerboard.gif');
 
     // Grab the actual content
-    var actualBytes = Buffer.concat(this.buffers);
+    var actualBytes = this.gif.read();
 
     // DEV: Write out actual file to expected file
     if (process.env.DEBUG_TEST) {
