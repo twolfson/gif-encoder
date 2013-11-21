d66b29fcdd7bdc44ec358a4059f8c2a23ccdc51a
Todd Wolfson <todd@twolfson.com>
2013-11-21 03:54:35 -0800

    Updating all TODO notes

--- test/gif-encoder_benchmark.js
+++ test/gif-encoder_benchmark.js
@@ -2,13 +2,15 @@ var imageUtils = require('./utils/image');
 var GifEncoder = require('../');
 
 describe('A GifEncoder', function () {
+  // DEV: With byte-by-byte buffers, we run into a process.nextTick overflow with all the events
+  // Thus, frame based buffers win by default
   describe('encoding a bunch of frames to `data` events', function () {
     before(function createGifEncoder () {
       this.gif = new GifEncoder(200, 200);
       this.gif.writeHeader();
 
       // Pipe output to nowhere
-      // TODO: Do the same for .read()
+      // DEV: We should test .read() but we already have frame based buffers as our winner
       this.gif.on('data', function () {});
     });
     imageUtils.load('medium-size.png');
@@ -25,10 +27,9 @@ describe('A GifEncoder', function () {
     });
 
     it('can do so efficiently', function () {
-      // TODO: Move to ops/second via benchmarkjs?
+      // DEV: We should move to ops/second for other benchmarks but this test has a win-by-default
       // Medium size x 500 frames 13088 ms for frame based buffers
       // Medium size x 500 frames 14245 ms for byte by byte
-      // However, there is a process.nextTick overflow with all the events
       console.log('Medium size x 500 frames', this.totalTime + ' ms');
     });
   });
