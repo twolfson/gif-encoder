f8e20377c33106ad30e9b4408b637b6cbb0d5518
Todd Wolfson <todd@twolfson.com>
2015-12-19 20:39:47 -0600

    Modularized script

--- tmp.js
+++ tmp.js
@@ -2,20 +2,23 @@
 var Canvas = require('canvas');
 var GIFEncoder = require('./');
 var fs = require('fs');
-var canvas = new Canvas(10, 10),
+var WIDTH = 10;
+var HEIGHT = 10;
+var STEP = 10;
+var canvas = new Canvas(WIDTH, HEIGHT),
     ctx = canvas.getContext('2d');
 
-var encoder = new GIFEncoder(10,10);
+var encoder = new GIFEncoder(WIDTH, HEIGHT);
 encoder.pipe(fs.createWriteStream(__dirname + '/test.gif'));
 encoder.setRepeat(0);
 encoder.setDelay(50);
 // encoder.setTransparent(0xFF00FF);
 encoder.writeHeader();
-for(var i = 0; i < 10; i++) {
+for(var i = 0; i < STEP; i++) {
     ctx.fillStyle = '#FF00FF';
-    ctx.fillRect(0,0,10,10);
+    ctx.fillRect(0,0, WIDTH, HEIGHT);
     ctx.fillStyle = '#000000';
-    ctx.fillRect(i*1,4,1,1);
-    encoder.addFrame(ctx.getImageData(0, 0, 10, 10).data);
+    ctx.fillRect(i * 1 / STEP, HEIGHT * 4 / STEP, WIDTH / STEP, HEIGHT / STEP);
+    encoder.addFrame(ctx.getImageData(0, 0, WIDTH, HEIGHT).data);
 }
 encoder.finish();
