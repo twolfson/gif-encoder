ffac493214100d60eb2a3b24049f0dd646d11d25
Todd Wolfson <todd@twolfson.com>
2016-10-24 01:50:27 -0700

    Added transparent image test

--- test/gif-encoder_test.js
+++ test/gif-encoder_test.js
@@ -2,6 +2,8 @@ var assert = require('assert');
 var fs = require('fs');
 
 var async = require('async');
+var ndarrayFill = require('ndarray-fill');
+var zeros = require('zeros');
 
 var GifEncoder = require('../lib/GIFEncoder.js');
 var checkerboardPixels = require('./test-files/checkerboard-pixels.json');
@@ -88,5 +90,63 @@ describe('GifEncoder encoding an overly large, underly read checkerboard', funct
 
   it('emits an error', function () {
     assert.notEqual(this.error, undefined);
+  });
+});
+
+describe('GifEncoder encoding a multi-framed image with a transparent background', function () {
+  createGif(15, 15);
+  before(function () {
+    this.gif.writeHeader();
+    this.gif.setDelay(500);
+    this.gif.setTransparent(0xFF00FF);
+    this.gif.setRepeat(0);
+    var i = 0;
+    for (; i < 3; i++) {
+      // DEV: We are drawing a diagonally moving dot
+      // +------+    +------+    +------+
+      // |xx    |    |      |    |      |
+      // |xx    |    |      |    |      |
+      // |      | -> |  xx  | -> |      |
+      // |      | -> |  xx  | -> |      |
+      // |      |    |      |    |    xx|
+      // |      |    |      |    |    xx|
+      // +------+    +------+    +------+
+
+      // Generate our frame
+      var frameNdarray = zeros([15, 15, 4]);
+      ndarrayFill(frameNdarray, function fillFrame (x, y, rgbaIndex) {
+        // If this is the alpha channel, always return it as full
+        if (rgbaIndex === 3) {
+          return 0xFF;
+        }
+
+        // Otherwise, if we are on our black dot, then draw it
+        if (i * 5 <= x && x < (i + 1) * 5 &&
+            i * 5 <= y && y < (i + 1) * 5) {
+          // Generate black dot (00 00 00)
+          return 0x00;
+        // Otherwise, draw our transparent color
+        } else {
+          return rgbaIndex === 1 ? 0x00 : 0xFF;
+        }
+      });
+
+      // Draw our frame
+      this.gif.addFrame(frameNdarray.data);
+    }
+    this.gif.finish();
+  });
+  before(function (done) {
+    this.gif.once('readable', done);
+  });
+
+  it('generates the expected bytes', function () {
+    var expectedBytes = fs.readFileSync(__dirname + '/expected-files/moving-dot.gif');
+    var actualBytes = this.gif.read();
+    if (process.env.DEBUG_TEST) {
+      try { fs.mkdirSync(__dirname + '/actual-files'); } catch (e) {}
+      fs.writeFileSync(__dirname + '/actual-files/moving-dot.gif', actualBytes);
+    }
+    assert.deepEqual(expectedBytes, actualBytes);
   });
 });
